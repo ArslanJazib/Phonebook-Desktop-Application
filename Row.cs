b454dcd3564c0ae66c6f3d47eb8b8d204d72d0c5
Arslan Jazib <58139645+ArslanJazib@users.noreply.github.com>
2021-07-27 19:46:24 +0500

    Add files via upload

---
+++ Row.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTier
+{
+    public class Row
+    {
+        string serial;
+        string name;
+        string number;
+        string email;
+        string address;
+        public string Serial { get => serial; set => serial = value; }
+        public string Name { get => name; set => name = value; }
+        public string Number { get => number; set => number = value; }
+        public string Email { get => email; set => email = value; }
+        public string Address { get => address; set => address = value; }
+    }
+}
