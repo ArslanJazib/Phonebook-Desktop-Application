e0d7f54492ac668f2463f89e9f4a04116e8c2241
Arslan Jazib <58139645+ArslanJazib@users.noreply.github.com>
2021-07-27 19:47:31 +0500

    Add files via upload

---
+++ Data Tier/FileHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+namespace DataTier
+{
+    public class FileHandler
+    {
+        static bool fileStatus = false;
+        List<Row> AllRows = new List<Row>();
+        public List<Row> LoadData()
+        {
+            StreamReader reader = new StreamReader("Phonebook.txt");
+            while (!reader.EndOfStream)
+            {
+                Row temporaryRow = new Row();
+                temporaryRow.Serial = reader.ReadLine();
+                temporaryRow.Name = reader.ReadLine();
+                temporaryRow.Number = reader.ReadLine();
+                temporaryRow.Email = reader.ReadLine();
+                temporaryRow.Address = reader.ReadLine();
+                AllRows.Add(temporaryRow);
+            }
+            reader.Close();
+            return AllRows;
+        }
+        public void StoreData(Row tempRow)
+        {
+            if(fileStatus==false)
+            {
+                StreamWriter temp = new StreamWriter("Phonebook.txt");
+                temp.Close();
+            }
+            StreamWriter writer = new StreamWriter("Phonebook.txt",true);
+            writer.WriteLine(tempRow.Serial);
+            writer.WriteLine(tempRow.Name);
+            writer.WriteLine(tempRow.Number);
+            writer.WriteLine(tempRow.Email);
+            writer.WriteLine(tempRow.Address);
+            writer.Close();
+            fileStatus = true;
+        }
+    }
+}
